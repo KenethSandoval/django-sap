e26866dfd332999d0ce14e09bd4aff66dd812bb3
KenethSandoval <kenethra.74@gmail.com>
2020-11-27 15:26:55 -0600

    [feat]: templates

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def bienvenido(request):
-    return HttpResponse('Hola mundo desde Django')
+    mensajes = {
+        'msg1': 'Valor mensaje 1'
+    }
+    return render(request, 'bienvenida.html', mensajes)
 
 def contacto(request):
     return HttpResponse('Email: kenetrha.74@gmail.com\n Telefono: 51296960')
