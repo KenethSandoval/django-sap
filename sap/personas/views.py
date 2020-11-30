f6c30ee19fd9c96d96b720b5d1d0089768141de8
KenethSandoval <kenethra.74@gmail.com>
2020-11-30 14:27:42 -0600

    [feat]: CRUD completed

--- sap/personas/views.py
+++ sap/personas/views.py
@@ -4,21 +4,50 @@ from personas.models import Persona
 from personas.forms import PersonaForm
 # Create your views here.
 
+
 def detallePersona(request, id):
     persona = get_object_or_404(Persona, pk=id)
     return render(request, 'personas/detalle.html', {'persona': persona})
 
+
 def nuevaPersona(request):
     if request.method == 'POST':
-       formaPersona = PersonaForm(request.POST)
-       if formaPersona.is_valid():
-           formaPersona.save() 
-           return redirect('index')
+        formaPersona = PersonaForm(request.POST)
+        if formaPersona.is_valid():
+            formaPersona.save()
+            return redirect('index')
     else:
         formaPersona = PersonaForm()
 
     return render(request, 'personas/nuevo.html', {'formaPersona': formaPersona})
 
+
 def editarPersona(request, id):
     persona = get_object_or_404(Persona, pk=id)
-    return render(request, 'personas/detalle.html', {'persona', persona})
+
+    if request.method == 'POST':
+        formaPersona = PersonaForm(request.POST, instance=persona)
+        if formaPersona.is_valid():
+            formaPersona.save()
+            return redirect('index')
+    else:
+        formaPersona = PersonaForm(instance=persona)
+
+    return render(request, 'personas/editar.html', {'formaPersona': formaPersona})
+
+
+def eliminarPersona(request, id):
+    persona = get_object_or_404(Persona, pk=id)
+    if persona:
+        persona.delete()
+        return redirect('index')
+
+    if request.method == 'POST':
+        formaPersona = PersonaForm(request.POST, instance=persona)
+        if formaPersona.is_valid():
+            formaPersona.save()
+            return redirect('index')
+    else:
+        formaPersona = PersonaForm(instance=persona)
+
+    return render(request, 'personas/editar.html', {'formaPersona': formaPersona})
--- sap/sap/urls.py
+++ sap/sap/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 
 from webapp.views import bienvenido
-from personas.views import detallePersona, nuevaPersona, editarPersona
+from personas.views import detallePersona, nuevaPersona, editarPersona, eliminarPersona
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', bienvenido, name='index'),
     path('detalle_persona/<int:id>', detallePersona),
     path('nueva_persona', nuevaPersona),
-    path('editar_persona/<int:id>', editarPersona)
+    path('editar_persona/<int:id>', editarPersona),
+    path('eliminar_persona/<int:id>', eliminarPersona)
 ]
