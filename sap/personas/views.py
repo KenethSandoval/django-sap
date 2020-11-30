bb8c9359315ee97d732760ec9edc52dc9f5e8eee
KenethSandoval <kenethra.74@gmail.com>
2020-11-30 12:26:44 -0600

    [feat]: Method to edit person

--- sap/personas/views.py
+++ sap/personas/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.forms import modelform_factory
 from personas.models import Persona
-
+from personas.forms import PersonaForm
 # Create your views here.
 
 def detallePersona(request, id):
     persona = get_object_or_404(Persona, pk=id)
     return render(request, 'personas/detalle.html', {'persona': persona})
 
-PersonaForm = modelform_factory(Persona, exclude=[])
-
 def nuevaPersona(request):
-    formaPersona = PersonaForm()
+    if request.method == 'POST':
+       formaPersona = PersonaForm(request.POST)
+       if formaPersona.is_valid():
+           formaPersona.save() 
+           return redirect('index')
+    else:
+        formaPersona = PersonaForm()
+
     return render(request, 'personas/nuevo.html', {'formaPersona': formaPersona})
+
+def editarPersona(request, id):
+    persona = get_object_or_404(Persona, pk=id)
+    return render(request, 'personas/detalle.html', {'persona', persona})
